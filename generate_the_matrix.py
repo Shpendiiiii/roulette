83845b053b643b40dd8e145608b3412878f5bfa7
Shpendiiii <shpendaliu69@gmail.com>
2023-08-14 20:18:01 +0200

    changes to matrix generating file

--- generate_the_matrix.py
+++ generate_the_matrix.py
@@ -1,3 +1,5 @@
+import time
+
 from rich.color import ColorParseError
 from rich.console import Console
 
@@ -14,6 +16,13 @@ def generate_pure_matrix():
     return main
 
 
+def generate_matrix_style(pure_matrix, style="EU"):
+    pure_matrix.insert(0, [0])
+    if style == "US":
+        pure_matrix.insert(1, ["00"])
+    return pure_matrix
+
+
 def generate_colors(matrix):
     """
        Generate the reds and blacks for the matrix
@@ -24,41 +33,42 @@ def generate_colors(matrix):
     red = "[red]"
     black = "[black]"
     is_red = True
-
     color_dict = {}
-
     for row in matrix:
         for item in row:
             color = black if is_red else red
-            color_dict.update({f"{item}": f"{color}"})
+            if item == 0 or item == "00":
+                color_dict.update({f"{str(item)}": "[green]"})
+            else:
+                color_dict.update({f"{str(item)}": f"{color}"})
             is_red = not is_red
     return color_dict
-
-
-def generate_matrix_style(pure_matrix):
-    pure_matrix.update({"00": "[green]"})
-    return pure_matrix
 
 
 def pretty_print_matrix(dictionary):
     """
     Print a matrix in a pretty format.
 
-    Args:
+    Args: 
         matrix: The matrix to print.
     """
     console = Console()
-
     i = 0
-
     for key, value in dictionary.items():
-        if i % 3 == 0:
+        if i != 0 and i % 3 == 0:
             print("\n")
-        space = "\n"
-
-        if len(key) < 2:
+        if key == "0" or key == "00":
+            if "00" not in dictionary:
+                console.print(f"[green]{' ' * 6}{key}")
+                # print("\n")
+            else:
+                console.print(f"[green]{' ' * 3}{key}", end="")
+            i -= 1
+        if len(key) < 2 and key != "0" and key != "00":
+            if i == 0 and "00" in dictionary:
+                print("\n")
             console.print(f"{value}  {key}", end=" ")
-        else:
+        elif key != "0" and key != "00":
             console.print(f"{value} {key}", end=" ")
         i += 1
 
@@ -66,4 +76,12 @@ def pretty_print_matrix(dictionary):
 # pure_matrix = generate_pure_matrix()
 # dict_with_colors = generate_colors(pure_matrix)
 # final = pretty_print_matrix(dict_with_colors)
-pretty_print_matrix(generate_colors(generate_pure_matrix()))
+pretty_print_matrix(generate_colors(generate_matrix_style(generate_pure_matrix())))
+print("\n")
+pretty_print_matrix(generate_colors(generate_matrix_style(generate_pure_matrix(), "US")))
+
+
+
+
+
+# print((generate_matrix_style(generate_pure_matrix())))
