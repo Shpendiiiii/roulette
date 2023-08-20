9645e54a148d0584e6311fb3cbb93e5170c2b027
Shpendiiii <shpendaliu69@gmail.com>
2023-08-20 11:57:36 +0200

    experiments with threading and user input

--- test.py
+++ test.py
@@ -1,12 +1,43 @@
-import rich
+from rich import print
+from rich.text import Text
+import threading
+import time
+import msvcrt
 
+exit_flag = False
 
-def print_in_bright_baby_blue(text):
+def monitor_exit_condition():
+    global exit_flag
+    while not exit_flag:
+        if msvcrt.kbhit():  # Check if a key was pressed
+            key = msvcrt.getch().decode('utf-8')  # Get the pressed key
+            if key.lower() == "exit":
+                exit_flag = True
+                print("Exiting the program.")
+
+def print_in_bright_baby_blue():
     """Prints the given text in bright baby blue."""
-    rich.print(
-        f"This text is in bright baby blue: {text}", style=rich.color.rgb(87, 192, 250)
+    styled_text = Text.assemble(
+        "This text is in bright baby blue:", style="rgb(175, 0, 255)"
     )
+    print(styled_text)
+    ask_user = input("What is your fav color of the alphabet: ")
+    styled_text_2 = Text.assemble(ask_user, style="blink magenta")
+    print(styled_text_2)
 
+def main():
+    while not exit_flag:
+        print_in_bright_baby_blue()
+        time.sleep(1)  # Simulating some work
 
 if __name__ == "__main__":
-    print_in_bright_baby_blue("Hello, shpend!")
+    exit_thread = threading.Thread(target=monitor_exit_condition)
+    main_thread = threading.Thread(target=main)
+
+    exit_thread.start()
+    main_thread.start()
+
+    exit_thread.join()
+    main_thread.join()
+
+    print("Program has been gracefully exited.")
